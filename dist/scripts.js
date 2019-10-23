ace6c10062449d77f99f764625948434078be9fd
kevmhughes <kevhughes24@hotmail.com>
2019-10-23 17:36:12 +0200

    added minified files to dist

---
+++ dist/scripts.js
@@ -0,0 +1,10 @@
+(function(){var pokemonRepository=(function(){var repository=[];var apiUrl='https://pokeapi.co/api/v2/pokemon/?limit=150';function getAll(){return repository}
+function add(item){repository.push(item)}
+function loadList(){return $.ajax(apiUrl,{dataType:'json'}).then(function(item){$.each(item.results,function(index,item){var pokemon={name:item.name,detailsUrl:item.url}
+add(pokemon)})}).catch(function(e){console.error(e)})}
+function loadDetails(item){var url=item.detailsUrl;return $.ajax(url).then(function(details){item.imageUrl=details.sprites.front_default;item.height=details.height;item.weight=details.weight;item.types=details.types.map(function(pokemon){return pokemon.type.name})}).catch(function(e){console.error(e)})}
+return{add:add,getAll:getAll,loadList:loadList,loadDetails:loadDetails}})();var $pokemonList=$('.pokemon-list')
+function addListItem(pokemon){var listItem=$('<button type="button" class="pokemon-list_item list-group-item list-group-item-action" data-toggle="modal" data-target="#pokemon-modal"></button>');listItem.text(pokemon.name);$pokemonList.append(listItem);listItem.click(function(){showDetails(pokemon)})}
+function showDetails(pokemon){pokemonRepository.loadDetails(pokemon).then(function(){var modal=$('.modal-body');var name=$('.modal-title').text(pokemon.name);var height=$('<p class="pokemon-height"></p>').text('Height: '+pokemon.height+' decimetres.');var weight=$('<p class="pokemon-weight"></p>').text('Weight: '+pokemon.weight+' hectograms.');var type=$('<p class="pokemon-type"></p>').text('Type: '+pokemon.types+'.');var image=$('<img class="pokemon-picture">');image.attr('src',pokemon.imageUrl);if(modal.children().length){modal.children().remove()}
+modal.append(image).append(height).append(weight).append(type)})}
+$(document).ready(function(){$('#pokemon-search').on('keyup',function(){var value=$(this).val().toLowerCase();$('.pokemon-list_item').filter(function(){$(this).toggle($(this).text().toLowerCase().indexOf(value)>-1)})})});pokemonRepository.loadList().then(function(){var pokemons=pokemonRepository.getAll();$.each(pokemons,function(index,pokemon){addListItem(pokemon)})})})()
